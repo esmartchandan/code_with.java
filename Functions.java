3638a808834fc031910dfd916d09aca546b9afc5
Chandan Kumar Shaw <147383845+esmatChandan@users.noreply.github.com>
2024-03-16 08:56:25 +0530

    Add files via upload

---
+++ Functions.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Functions {
+    public static int calculateProduct(int a, int b){
+ int sum = a * b;
+ return sum;
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        
+        int sum  = calculateProduct(a,b);
+        System.out.print("product of 2 number is "+ sum);
+}
+}
