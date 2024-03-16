c52cf37ae5d373dd265e9a15c439ee311a367abe
Chandan Kumar Shaw <147383845+esmatChandan@users.noreply.github.com>
2024-03-16 08:57:08 +0530

    Update Functions.java

--- Functions.java
+++ Functions.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class Functions {
     public static int calculateProduct(int a, int b){
- int sum = a * b;
+ int sum = a + b;
  return sum;
     }
     public static void main(String args[]){
